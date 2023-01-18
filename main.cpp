ff3c3b6a0e2d99db6cbc3dad3842355d9f51c248
SantikMaster <SantikMaster@gmail.com>
2023-01-18 14:03:02 +0200

    constraints added

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 #include <iostream>
 #include <vector>
 
-
-//#include "src/OpenMesh/Core/IO/MeshIO.hh"
-//#include "OpenMesh/Core/IO/MeshIO.hh" //?
-
-
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Core/IO/Options.hh>
 #include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 #include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
-//typedef
-using MyMesh = OpenMesh::PolyMesh_ArrayKernelT<>;
 
+using MyMesh = OpenMesh::PolyMesh_ArrayKernelT<>;
 
 struct float3 { float x, y, z; };
 struct int3 { int i0, i1, i2; };
@@ -21,32 +15,37 @@ struct Mesh {
     std::vector<float3> positions;
     std::vector<int3> triangles;
 };
-struct AABB {
+struct Box
+{
     float3 min;
     float3 max;
+    bool inside;
+    Box()
+    {
+        min.x = 0;
+        min.y = 0;
+        min.z = 0;
+
+        inside = true;
+
+        max.x = 1;
+        max.y = 1;
+        max.z = 1;
+    }
+
 };
-struct AABB_Flag {
-    AABB box;
-    bool inside_flag;
-};
-struct Input {
+
+struct Input 
+{
     Mesh mesh;
-    std::vector<AABB_Flag> input_data;
 };
-/*struct Output {
-    std::vector<Mesh> output_data;
-};*/
-struct Output {
+struct Output 
+{
     Mesh mesh;
 };
 
-// implement these methods
 void read(Input& input)
 {
-    // read test data from test_data files
-    // mesh                      - mesh.obj
-    // axis-Aligned Bounding Box - aabb*.txt
-  
     MyMesh mesh;
 
     auto test = OpenMesh::IO::read_mesh(mesh, "../bunny.obj");
@@ -71,27 +70,23 @@ void read(Input& input)
             pnt.x = point[0];
             pnt.y = point[1];
             pnt.z = point[2];
-       //     if (pnt.x < 0)
+
             {
                 input.mesh.positions.emplace_back(std::move(pnt));
-          //      std::cout << pnt.x << " " << pnt.y << " " << pnt.z << "\n";
-            }
-                 
+            }                
         }
     }
-
-   // auto test_write = OpenMesh::IO::write_mesh(mesh, "../edited_bunny.obj");
-   // mesh.vertex();
 }
 
 void write(Output& output) 
 {
     // save meshes from output.output_data to separate files
-    // file format - "mesh_result_{idx}.obj"
-  //  for (auto vi = output.output_data.begin(); vi != output.output_data.end(); ++vi)
     MyMesh mesh;
     MyMesh::VertexHandle vhandle;
     std::vector<MyMesh::VertexHandle> face_vhandles;
+
+    float min_distance = 100.0;
+    auto first = output.mesh.positions.front();
     for (auto vi = output.mesh.positions.begin(); vi != output.mesh.positions.end(); ++vi)
     {
         float3 pnt;
@@ -99,14 +94,16 @@ void write(Output& output)
 
         mesh.add_vertex(MyMesh::Point(pnt.x , pnt.y, pnt.z ));
 
+       
+        float distance = sqrt ((first.x - pnt.x) * (first.x - pnt.x) +
+                               (first.y - pnt.y) * (first.y - pnt.y) + 
+                               (first.z - pnt.z) * (first.z - pnt.z)) ;
+        if (distance < min_distance)  min_distance = distance;
     }
-    for (auto vi = mesh.vertices_begin(); vi+3 != mesh.vertices_end(); ++vi)
-    {
-       // auto point = mesh.point(*vi);
 
-        mesh.add_face(*vi, *(vi + 1), *(vi + 2));
- //       mesh.faces_begin
-    }
+    // TODO add faces between neighbors
+  //  for (auto vi = mesh.vertices_begin(); vi != mesh.vertices_end(); ++vi)
+
 
     OpenMesh::IO::Options OptionRead(OpenMesh::IO::Options::VertexTexCoord);
 
@@ -122,43 +119,47 @@ void write(Output& output)
         std::cout << "Normals are present";
     }
 
-    auto test_write = OpenMesh::IO::write_mesh(mesh, "../edited_bunny_2.obj");
+    auto test_write = OpenMesh::IO::write_mesh(mesh, "../edited_bunny_3.obj");
     std::cout << "writed new obj\n";
  
 }
 
-void intersect(const Mesh& inputMesh, const AABB_Flag& inputAABBFlag, Mesh& outputMesh)
-{
-    // implement intersection operation using only one current thread, processing each triangle one by one sequentially
-    // print runtime to console
-    
-}
-void intersect(const Mesh& inputMesh, Mesh& outputMesh)
-{
-    // implement intersection operation using only one current thread, processing each triangle one by one sequentially
-    // print runtime to console
-    outputMesh = inputMesh;
-}
-void intersectFast(const Mesh& inputMesh, const AABB_Flag& inputAABBFlag, Mesh& outputMesh) 
+void intersect(const Input& input, const Box& ConstraintBox, Output& output)
 {
     // implement intersection operation.
-    // Should work as fast as possible, using any tricks for speedup 
-    // (additional structures, fast search in space, some fast filter unmatched triangles, multithreading, etc)
-    // print runtime to console
+    MyMesh::VertexHandle vhandle;
+    std::vector<MyMesh::VertexHandle> face_vhandles;
+    for (auto vi = input.mesh.positions.begin(); vi != input.mesh.positions.end(); ++vi)
+    {
+        float3 pnt;
+        pnt = (*vi);
 
+
+        if (ConstraintBox.inside == true &&
+            (pnt.x > ConstraintBox.min.x && pnt.x < ConstraintBox.max.x) &&
+            (pnt.y > ConstraintBox.min.y && pnt.y < ConstraintBox.max.y) &&
+            (pnt.z > ConstraintBox.min.z && pnt.z < ConstraintBox.max.z)
+            )
+        {
+            output.mesh.positions.push_back(pnt);
+            std::cout << "pnt:" << pnt.x << ' ' << pnt.y << ' ' << pnt.z << "\n";
+        }
+           
+        if (ConstraintBox.inside == false &&
+            ((pnt.x < ConstraintBox.min.x || pnt.x > ConstraintBox.max.x) ||
+            (pnt.y < ConstraintBox.min.y || pnt.y > ConstraintBox.max.y) ||
+            (pnt.z < ConstraintBox.min.z || pnt.z > ConstraintBox.max.z)
+            ))
+        {
+            output.mesh.positions.push_back(pnt);
+            std::cout << "pnt:" << pnt.x << ' ' << pnt.y << ' ' << pnt.z << "\n";
+        }         
+    }
 }
 
 void intersect(const Input& input, Output& output)
 {
-   // output.output_data.resize(input.input_data.size());
-    int outputIdx = 0;
-    intersect(input.mesh, output.mesh);
-
-   /* for (auto& inputData : input.input_data)
-    {
-     //   intersect(input.mesh, inputData, output.output_data[outputIdx++]);
-        //intersectFast(input.mesh, inputData, output.output_data[outputIdx++]);
-    }*/
+    output.mesh = input.mesh;
 }
 
 void main()
@@ -167,8 +168,14 @@ void main()
     read(input);
 
     Output output;
-    intersect(input, output);
+
+    Box Constraints;
+    Constraints.max.x = 1;
+    Constraints.max.y = 1;
+    Constraints.max.z = 1;
+    Constraints.inside = false;
+
+    intersect(input, Constraints, output);
 
     write(output);
-  
 }
